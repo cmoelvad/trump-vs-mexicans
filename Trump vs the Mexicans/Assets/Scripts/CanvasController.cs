3868d820a3583b832193687149734721c47834e7
Unknown <bettebirch@hotmail.com>
2019-04-11 00:20:21 +0200

    Fixed leaderboard and usa-trigger

--- Trump vs the Mexicans/Assets/Scripts/CanvasController.cs
+++ Trump vs the Mexicans/Assets/Scripts/CanvasController.cs
@@ -29,7 +29,9 @@ public class CanvasController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Cancel") && GameController.GetGameState() == GameController.GameState.PLAYING)
+       
+
+        if (Input.GetButton("Cancel") && GameController.GetGameState() == GameController.GameState.PLAYING)
         {
             mainMenu.gameObject.SetActive(true);
             Time.timeScale = 0;
@@ -58,23 +60,23 @@ public class CanvasController : MonoBehaviour
 
             finalScore.text = "Your score was " + (enemySpawnObj.healthToAdd + 500) + "\n\n";
 
-                StartCoroutine(PutScoreToLeaderboard(new ScoreObject
-                {
-                    name = playerName.text,
-                    score = (enemySpawnObj.healthToAdd + 500)
-                }));
+            var name = playerName.text;
+            StartCoroutine(PutScoreToLeaderboard(new ScoreObject
+            {
+                name = name,
+                score = (enemySpawnObj.healthToAdd + 500)
+            }));
                 
+            scores.Sort();
+            scores.Reverse();
 
-               
-                scores.Sort();
-                scores.Reverse();
-
-                int ranking = 1;
-                foreach (var score in scores)
-                {
-                    finalScore.text += ranking + ") " + score.name + " (" + score.score + ") \n";
-                    ranking++;
-                }
+                Debug.Log("size of array: "+scores.ToArray().Length);
+            int ranking = 1;
+            foreach (var score in scores)
+            {
+                finalScore.text += ranking + ") " + score.name + " (" + score.score + ") \n";
+                ranking++;
+            }
 
             }
         else
@@ -97,6 +99,7 @@ public class CanvasController : MonoBehaviour
 
     private IEnumerator GetLeaderboard()
     {
+        Debug.Log("get leaderboard");
         string url = "http://mbirch.dk/trump/api/index.php?method=get";
         UnityWebRequest www = UnityWebRequest.Get(url);
         yield return www.SendWebRequest();
@@ -108,20 +111,22 @@ public class CanvasController : MonoBehaviour
         else
         {
             // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            //Debug.Log(www.downloadHandler.text);
             var line = www.downloadHandler.text;
 
-            var dataArray = line.Split(' ');
+            var dataArray = line.Split(';');
+            Debug.Log("leaderboard contained: " + dataArray.Length + " pairs");
             foreach (string data in dataArray)
             {
-                var scoreName = data.Split(',');
-                Debug.Log(data);
+                var scoreName = data.Split('[');
+                //Debug.Log(data);
 
                 if (data == "" || data == " ")
-                {
+                {   
                     continue;
                 }
-
+                //Debug.Log("from string to int... score: " + scoreName[0]);
+                //Debug.Log("name: " + scoreName[1]);
                 scores.Add(new ScoreObject
                 {
                     name = scoreName[1],
@@ -135,8 +140,8 @@ public class CanvasController : MonoBehaviour
     }
     private IEnumerator PutScoreToLeaderboard(ScoreObject scoreObject)
     {
-        string url = "http://mbirch.dk/trump/api/index.php?method=put&score="+scoreObject.score + "," + scoreObject.name;
-
+        string url = "http://mbirch.dk/trump/api/index.php?method=put&score="+scoreObject.score + "[" + scoreObject.name;
+        //Debug.Log("url: " + url);
         UnityWebRequest www = UnityWebRequest.Get(url);
         yield return www.SendWebRequest();
 
@@ -147,7 +152,7 @@ public class CanvasController : MonoBehaviour
         else
         {
             // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            //Debug.Log(www.downloadHandler.text);
 
             // Or retrieve results as binary data
             byte[] results = www.downloadHandler.data;
